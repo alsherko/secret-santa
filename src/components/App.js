2127d16c056083b87f492b336c1c027e6a2604b4
alsh <alsh@epolsoft.com>
2020-12-20 02:02:40 +0300

    add gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,42 @@
-import { Main } from './Main'
+import {useState} from "react";
+import {Main} from './Main'
+import {decryptHash} from "../common/utils";
 
 const App = () => {
-  return (
-    <>
-      <header>
-        <h1>
-          Тайный Санта
-          <span role="presentation" aria-hidden="true">
+    // const [params, setParams] = useState(getSearchParameters())
+
+    const transformToArray = (prmstr) => {
+        let params = {};
+        let prmarr = prmstr.split("&");
+        for ( let i = 0; i < prmarr.length; i++) {
+            let tmparr = prmarr[i].split("=");
+            params[tmparr[0]] = tmparr[1];
+        }
+        return params;
+    }
+
+    const getSearchParameters = () => {
+        let prmstr = window.location.search.substr(1);
+        return prmstr !== null && prmstr !== "" ? transformToArray(prmstr) : {};
+    }
+
+    const params = getSearchParameters();
+
+    console.log(params.giver, decryptHash(params.key))
+
+    return (
+        <>
+            <header>
+                <h1>
+                    Тайный Санта
+                    <span role="presentation" aria-hidden="true">
             🎅🏼
           </span>
-        </h1>
-      </header>
-      <Main />
-    </>
-  )
+                </h1>
+            </header>
+            <Main/>
+        </>
+    )
 }
 
 export default App
