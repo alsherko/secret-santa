8065a1ab0c48565a5434810b9bb134549437975b
alsh <alsh@epolsoft.com>
2020-12-20 02:12:44 +0300

    delete unused code & fix gitignore

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,6 @@
-import {useState} from "react";
 import {Main} from './Main'
-import {decryptHash} from "../common/utils";
 
 const App = () => {
-    // const [params, setParams] = useState(getSearchParameters())
-
-    const transformToArray = (prmstr) => {
-        let params = {};
-        let prmarr = prmstr.split("&");
-        for ( let i = 0; i < prmarr.length; i++) {
-            let tmparr = prmarr[i].split("=");
-            params[tmparr[0]] = tmparr[1];
-        }
-        return params;
-    }
-
-    const getSearchParameters = () => {
-        let prmstr = window.location.search.substr(1);
-        return prmstr !== null && prmstr !== "" ? transformToArray(prmstr) : {};
-    }
-
-    const params = getSearchParameters();
-
-    console.log(params.giver, decryptHash(params.key))
-
     return (
         <>
             <header>
