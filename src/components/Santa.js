c9617794fa0404f5a2ae36399fae130a07663a9b
alsh <alsh@epolsoft.com>
2020-12-20 12:23:10 +0300

    add receiver as link to instagram

--- src/components/Santa.js
+++ src/components/Santa.js
@@ -1,13 +1,13 @@
 import './santa.css'
 
-const Santa = ({ giver, reciver}) => {
+const Santa = ({ giver, reciver }) => {
   return (
     <div className="container">
       <div className="note">
         <h4 className="note__title">Здравствуй, {giver}</h4>
         <h4>
           Вас назначили тайным сантой для{' '}
-          <span className="note__santa">{reciver}</span>.
+          <a href={`https://www.instagram.com/${reciver}/`}>{reciver}</a>
         </h4>
         <h4>Удачи!</h4>
       </div>
